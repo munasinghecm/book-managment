c1cf451197ac0f49d70ad693f9c5f8c7f95e0047
chandanamunasinghe <chandana@synergentl.com>
2024-11-03 19:46:16 +0530

    fix form field border issue

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { BookListComponent } from './pages/book-list/book-list.component';
+import { AddBookComponent } from './pages/add-book/add-book.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'home', component: BookListComponent },
+    { path: 'add-book', component: AddBookComponent },
+    { path: '**', redirectTo: '/home' }
+];
